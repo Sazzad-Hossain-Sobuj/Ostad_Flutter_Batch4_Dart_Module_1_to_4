d6d5844c7c58f1ee29eb2aabdf2baf72de9fa816
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-09-05 10:24:40 +0600

    initial commit

---
+++ module1to4/dart_m1_to_m4_all/bin/dart_m1_to_m4_all.dart
@@ -0,0 +1,5 @@
+import 'package:dart_m1_to_m4_all/dart_m1_to_m4_all.dart' as dart_m1_to_m4_all;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_m1_to_m4_all.calculate()}!');
+}
---
+++ module1to4/dart_m1_to_m4_all/test/dart_m1_to_m4_all_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_m1_to_m4_all/dart_m1_to_m4_all.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

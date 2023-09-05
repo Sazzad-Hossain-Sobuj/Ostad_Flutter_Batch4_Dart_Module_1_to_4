652a9d7a641b26a3e60a1352d627076b981b166d
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-09-05 10:36:12 +0600

    module 1 live test

---
+++ module1to4/dart_m1_to_m4_all/Dart_Module_1_all/Dart_Module_1_LiveTest/live_test.dart
@@ -0,0 +1,12 @@
+void main() {
+  var persons = {
+    'PersonID': 10,
+    'Name': 'Sazzad Hosain',
+    'Age': 24,
+    'Salary': 25000,
+    'Gender': 'Male',
+  };
+
+  var salary = persons['Salary'];
+  print('Salary: $salary');
+}

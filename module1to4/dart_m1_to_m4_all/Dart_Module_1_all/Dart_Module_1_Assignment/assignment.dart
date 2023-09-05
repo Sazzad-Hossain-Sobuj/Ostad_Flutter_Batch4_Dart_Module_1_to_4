882b9caaa33680b3a192f85c7558af949a603e45
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-09-05 10:32:33 +0600

    module 1 assignment

---
+++ module1to4/dart_m1_to_m4_all/Dart_Module_1_all/Dart_Module_1_Assignment/assignment.dart
@@ -0,0 +1,10 @@
+void main(){
+  const int a = 20;
+  const int b = 10;
+  print("a = $a, b = $b\n" +
+      "Addition: $a + $b = ${a + b}\n" +
+      "Subtraction: $a - $b = ${a - b}\n" +
+      "Multiplication: $a * $b = ${a * b}\n" +
+      "Division: $a / $b = ${a / b}\n" +
+      "Modulus: $a % $b = ${a % b}");
+}
